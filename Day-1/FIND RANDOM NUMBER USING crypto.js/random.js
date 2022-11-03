3e51fff06a90ac4722c8b4303dd554432c09b3fe
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-11-04 02:07:18 +0530

    exporting randomInt module

---
+++ Day-1/FIND RANDOM NUMBER USING crypto.js/random.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+
+function randomInt(start, end) {
+  //crypto=> yeh start and end k andar se koi bhi random value dega in output
+  //so this the another way of finding random number using crypto and import export in Node.js
+  return crypto.randomInt(start,end);
+}
+module.exports = {
+  randomInt
+
+};
