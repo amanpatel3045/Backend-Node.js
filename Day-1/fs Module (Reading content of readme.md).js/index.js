13c5e2ccbe57c2a83d8d68d82b83c4ee17eb56b1
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-11-04 01:23:04 +0530

    Update index.js

--- Day-1/fs Module (Reading content of readme.md).js/index.js
+++ Day-1/fs Module (Reading content of readme.md).js/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const content =  fs.readFileSync('./Readme.md', {
+  //if encoding wali line (4th) na likhe toh output me buffer aayega (buffer means sequence of ASCII CODE) character ki jgh code me likha hua aayega in terminal
+  // agr encoding :'utf-8' likhenge toh output String me aayega.
   encoding: 'utf-8'
 })
 console.log(content);
