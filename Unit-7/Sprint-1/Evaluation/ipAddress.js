75d745d186effdf191b47ac5c43022b996ebbaf3
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-11-16 23:51:27 +0530

    Create ipAddress.js

---
+++ Unit-7/Sprint-1/Evaluation/ipAddress.js
@@ -0,0 +1,11 @@
+import dns from "dns"
+// const dns = require('dns'); 
+dns.lookup('masaischool.com', (err, value) => { 
+    if(err) { 
+        console.log(err); 
+        return; 
+    } 
+    console.log(value); 
+}
+
+)
